5b14e43274ddf6d5ebabfbffa4d37c341a9b9a1c
Giorgos Charitakis <chgiorgos13@gmail.com>
2018-01-06 22:12:07 +0200

    Added Carthage support

---
+++ XMLMapper/XMLMapper.h
@@ -0,0 +1,18 @@
+//
+//  XMLMapper.h
+//  XMLMapper
+//
+//  Created by Giorgos Charitakis on 06/01/2018.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XMLMapper.
+FOUNDATION_EXPORT double XMLMapperVersionNumber;
+
+//! Project version string for XMLMapper.
+FOUNDATION_EXPORT const unsigned char XMLMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMLMapper/PublicHeader.h>
+
+
