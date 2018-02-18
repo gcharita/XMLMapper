585115dd1c933ba765489cc504b3e3bbe331c8ad
Giorgos Charitakis <chgiorgos13@gmail.com>
2018-02-18 21:32:53 +0200

    Removed init(formatString:,locale:) from XMLCustomDateFormatTransform that causes ambiguous call

--- XMLMapper/Classes/XMLCustomDateFormatTransform.swift
+++ XMLMapper/Classes/XMLCustomDateFormatTransform.swift
@@ -10,14 +10,6 @@ import Foundation
 
 open class XMLCustomDateFormatTransform: XMLDateFormatterTransform {
     
-    public init(formatString: String, with locale: Locale = Locale(identifier: "en_US_POSIX")) {
-        let formatter = DateFormatter()
-        formatter.locale = locale
-        formatter.dateFormat = formatString
-        
-        super.init(dateFormatter: formatter)
-    }
-    
     public init(formatString: String, withLocaleIdentifier localeIdentifier: String = "en_US_POSIX") {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: localeIdentifier)
